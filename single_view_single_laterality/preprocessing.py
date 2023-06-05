d76dadd38afa243308e9644faacb9aed727d9711
sashank <sashanktalakola2@gmail.com>
2023-06-05 15:47:49 +0530

    updated image augmentation

--- single_view_single_laterality/preprocessing.py
+++ single_view_single_laterality/preprocessing.py
@@ -1,16 +1,18 @@
 import albumentations as A
 from albumentations.pytorch.transforms import ToTensorV2
-from albumentations import Resize
+from albumentations import Resize, Normalize
 
 def getTransforms(mode="TRAIN", IMG_SIZE_HEIGHT=768, IMG_SIZE_WIDTH=384):
     if mode == "TRAIN":
         transforms = A.Compose([
                 Resize(IMG_SIZE_HEIGHT, IMG_SIZE_WIDTH),
+                Normalize(mean=0.2179, std=0.0529),
                 ToTensorV2()
             ])
     else:
         transforms = A.Compose([
             Resize(IMG_SIZE_HEIGHT, IMG_SIZE_WIDTH),
+            Normalize(mean=0.2179, std=0.0529),
             ToTensorV2(),
         ])
     return transforms
